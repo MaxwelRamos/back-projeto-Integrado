9486ea6c76449a3af4252dd51b933be7a831b182
MaxwelRamos <gronesoft.atendimento@gmail.com>
2020-09-30 17:56:01 -0300

    Inclusao rota raiz

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+routes.get('/', (req, res) => {
+    res.send("Raiz");
+});
+
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
