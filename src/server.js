379dfc4b5e54a54c1241514a2f80fe34c8f95ded
MaxwelRamos <gronesoft.atendimento@gmail.com>
2020-10-30 14:35:22 -0300

    deploy heroku

--- src/server.js
+++ src/server.js
@@ -5,6 +5,16 @@ import app from './app';
 //     console.log("Servidor iniciado na porta 8080");
 // });
 
-app.listen(process.env.PORT || 8080, () => {
+require('dotenv').config({path: '.env'});
+console.log(process.env.HOST);
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8000;
+
+// app.listen(process.env.PORT || 8080, () => {
+//     console.log("Servidor iniciado.");
+// })
+
+app.listen(port, host, () => {
     console.log("Servidor iniciado.");
 })
