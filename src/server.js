ae2bba3cfb99dbfe8548b7db744243e77cb9600a
MaxwelRamos <gronesoft.atendimento@gmail.com>
2020-10-30 11:44:52 -0300

    configuracoes heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 //const app = require('./app');
 import app from './app';
 
-app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080");
-});
+// app.listen(8080, () => {
+//     console.log("Servidor iniciado na porta 8080");
+// });
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Servidor iniciado.");
+})
